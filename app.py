7eee49b740f29fe419e7de164db2900ebb22d2cb
hugopablo-ops <hugopablo@gmail.com>
2025-12-09 16:34:40 -0300

    Update app.py
    Optimización

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-# app.py
+from flask import Flask, render_template, request
 
-from flask import Flask
-
-# Creamos la aplicación
 app = Flask(__name__)
 
-# Esto define la página principal (la 'ruta' inicial)
+# Ruta 1: Muestra el formulario al entrar
 @app.route('/')
 def inicio():
-    return "<h1>¡Hola! Esta es mi App de Personal Trainer</h1><p>Estamos en construcción.</p>"
+    return render_template('index.html')
 
-# Esto enciende el servidor
+# Ruta 2: Recibe los datos cuando le das al botón "Guardar"
+@app.route('/subir', methods=['POST'])
+def subir_datos():
+    # Aquí es donde capturaremos el peso y la foto más adelante
+    return "<h1>¡Recibido!</h1><p>El sistema ha detectado el envío. (Pronto guardaremos la foto aquí).</p>"
+
 if __name__ == '__main__':
     app.run(debug=True)
